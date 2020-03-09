1a717a33898ec17f1ff812af8e924953f878d8df
Vova <vovaost61@gmail.com>
2020-03-09 23:36:42 +0200

    2 sections added

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,48 @@ document.querySelector(".btn").addEventListener("click", function() {
     .querySelector(".nav__opened-list")
     .classList.toggle("nav__opened-active");
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var btc = "https://api.cryptonator.com/api/ticker/btc-usd";
+  var eth = "https://api.cryptonator.com/api/ticker/eth-usd";
+  var ltc = "https://api.cryptonator.com/api/ticker/ltc-usd";
+
+  // New style
+  // fetch(btc).then(result => {
+  //   console.log(result);
+  // });
+
+  function getCurrency(url, callback) {
+    if (!url) return false;
+    // Old style
+    var request = new XMLHttpRequest();
+
+    request.addEventListener("load", function() {
+      var pars = JSON.parse(request.response);
+      var result = Math.floor(pars.ticker.price);
+
+      callback(result);
+    });
+
+    request.open("GET", url);
+    request.send();
+  }
+
+  getCurrency(btc, function(result) {
+    document
+      .querySelector("#btc")
+      .querySelector(".story__performance-price").innerText = "$ " + result;
+  });
+
+  getCurrency(eth, function(result) {
+    document
+      .querySelector("#eth")
+      .querySelector(".story__performance-price").innerText = "$ " + result;
+  });
+
+  getCurrency(ltc, function(result) {
+    document
+      .querySelector("#ltc")
+      .querySelector(".story__performance-price").innerText = "$ " + result;
+  });
+});
